d05d72a8f40eac5dbc4d5e800dc12742c93c385b
cfair <cfair@hach.com>
2014-06-24 14:06:41 -0600

    modified:   board-dt-sam9.c 	modified:   board-dt-sama5.c

--- arch/arm/mach-at91/board-dt-sama5.c
+++ arch/arm/mach-at91/board-dt-sama5.c
@@ -25,7 +25,231 @@
 
 #include "at91_aic.h"
 #include "generic.h"
+#include "clock.h"
 
+/************************************/
+/* TEMPORARY NON-DT STUFF FOR MIURA */
+/************************************/
+#include <linux/clk.h>
+#include <linux/delay.h>
+#include <linux/fb.h>
+#include <video/atmel_lcdfb.h>
+#include <mach/atmel_hlcdc.h>
+#include <media/soc_camera.h>
+#include <media/atmel-isi.h>
+
+#define LINK_SENSOR_MODULE_TO_SOC_CAMERA(_sensor_name, _soc_camera_id)	\
+	static struct soc_camera_desc iclink_##_sensor_name = {		\
+		.subdev_desc = {					\
+			.power = i2c_camera_power,			\
+			.query_bus_param = isi_camera_query_bus_param,	\
+		},							\
+		.host_desc = {						\
+			.bus_id		= -1,				\
+			.board_info	= &i2c_##_sensor_name,		\
+			.i2c_adapter_id	= 1,				\
+		},							\
+	};								\
+	static struct platform_device isi_##_sensor_name = {		\
+		.name	= "soc-camera-pdrv",				\
+		.id	= _soc_camera_id,				\
+		.dev	= {						\
+			.platform_data = &iclink_##_sensor_name,	\
+		},							\
+	};
+
+/*
+ * LCD Controller
+ */
+static struct fb_videomode at91_tft_vga_modes[] = {
+	{
+		.name		= "LG",
+		.refresh	= 60,
+		.xres		= 800,		.yres		= 480,
+		.pixclock	= KHZ2PICOS(33260),
+
+		.left_margin	= 88,		.right_margin	= 168,
+		.upper_margin	= 8,		.lower_margin	= 37,
+		.hsync_len	= 128,		.vsync_len	= 2,
+
+		.sync		= 0,
+		.vmode		= FB_VMODE_NONINTERLACED,
+	},
+};
+
+static struct fb_monspecs at91fb_default_monspecs = {
+	.manufacturer	= "LG",
+	.monitor	= "LB043WQ1",
+
+	.modedb		= at91_tft_vga_modes,
+	.modedb_len	= ARRAY_SIZE(at91_tft_vga_modes),
+	.hfmin		= 15000,
+	.hfmax		= 17640,
+	.vfmin		= 57,
+	.vfmax		= 67,
+};
+
+/* Default output mode is TFT 24 bit */
+#define BPP_OUT_DEFAULT_LCDCFG5	(LCDC_LCDCFG5_MODE_OUTPUT_24BPP)
+
+/* Driver datas */
+static struct atmel_lcdfb_info __initdata ek_lcdc_data = {
+	.lcdcon_is_backlight		= true,
+	.alpha_enabled			= false,
+	.default_bpp			= 16,
+	/* Reserve enough memory for 32bpp */
+	.smem_len			= 800 * 480 * 4,
+	/* default_lcdcon2 is used for LCDCFG5 */
+	.default_lcdcon2		= BPP_OUT_DEFAULT_LCDCFG5,
+	.default_monspecs		= &at91fb_default_monspecs,
+	.guard_time			= 9,
+	.lcd_wiring_mode		= ATMEL_LCDC_WIRING_RGB,
+};
+
+/*
+ *  ISI
+ */
+static struct isi_platform_data isi_data = {
+	.frate			= ISI_CFG1_FRATE_CAPTURE_ALL,
+	/* to use codec and preview path simultaneously */
+	.full_mode		= 1,
+	.data_width_flags	= ISI_DATAWIDTH_8 | ISI_DATAWIDTH_10,
+	/* ISI_MCK is provided by programmable clock or external clock */
+	.mck_hz			= 25000000,
+};
+
+static struct clk_lookup isi_mck_lookups[] = {
+	CLKDEV_CON_DEV_ID("isi_mck", "atmel_isi", NULL),
+};
+
+void __init at91_config_isi(bool use_pck_as_mck, const char *pck_id)
+{
+	struct clk *pck;
+	struct clk *parent;
+
+	if (use_pck_as_mck) {
+		pck = clk_get(NULL, pck_id);
+		parent = clk_get(NULL, "plla");
+
+		BUG_ON(IS_ERR(pck) || IS_ERR(parent));
+
+		if (clk_set_parent(pck, parent)) {
+			pr_err("Failed to set PCK's parent\n");
+		} else {
+			/* Register PCK as ISI_MCK */
+			isi_mck_lookups[0].clk = pck;
+			clkdev_add_table(isi_mck_lookups,
+				ARRAY_SIZE(isi_mck_lookups));
+		}
+
+		clk_put(pck);
+		clk_put(parent);
+	}
+}
+
+static unsigned int camera_reset_pin;
+static unsigned int camera_power_pin;
+static void camera_set_gpio_pins(uint reset_pin, uint power_pin)
+{
+	camera_reset_pin = reset_pin;
+	camera_power_pin = power_pin;
+}
+
+/*
+ * soc-camera OV2640
+ */
+static unsigned long isi_camera_query_bus_param(struct soc_camera_subdev_desc *link)
+{
+	/* ISI board for ek using default 8-bits connection */
+	return SOCAM_DATAWIDTH_8;
+}
+
+static int i2c_camera_power(struct device *dev, int on)
+{
+	int res, ret = 0;
+
+	pr_debug("%s: %s the camera\n", __func__, on ? "ENABLE" : "DISABLE");
+
+	res = devm_gpio_request(dev, camera_power_pin, "ov2640_power");
+	if (res < 0) {
+		printk("can't request ov2640_power pin\n");
+		return -1;
+	}
+
+	res = devm_gpio_request(dev, camera_reset_pin, "ov2640_reset");
+	if (res < 0) {
+		printk("can't request ov2640_reset pin\n");
+		devm_gpio_free(dev, camera_power_pin);
+		return -1;
+	}
+
+	/* enable or disable the camera */
+	res = gpio_direction_output(camera_power_pin, !on);
+	if (res < 0) {
+		printk("can't request output direction for ov2640_power pin\n");
+		ret = -1;
+		goto out;
+	}
+
+	if (!on)
+		goto out;
+
+	/* If enabled, give a reset impulse */
+	res = gpio_direction_output(camera_reset_pin, 0);
+	if (res < 0) {
+		printk("can't request output direction for ov2640_reset pin\n");
+		ret = -1;
+		goto out;
+	}
+	msleep(20);
+	res = gpio_direction_output(camera_reset_pin, 1);
+	if (res < 0) {
+		printk("can't request output direction for ov2640_reset pin\n");
+		ret = -1;
+		goto out;
+	}
+	msleep(100);
+
+out:
+	devm_gpio_free(dev, camera_reset_pin);
+	devm_gpio_free(dev, camera_power_pin);
+	return ret;
+}
+
+static struct i2c_board_info i2c_ov2640 = {
+	I2C_BOARD_INFO("ov2640", 0x30),
+};
+static struct i2c_board_info i2c_ov5642 = {
+	I2C_BOARD_INFO("ov5642", 0x3c),
+};
+static struct i2c_board_info i2c_ov9740 = {
+	I2C_BOARD_INFO("ov9740", 0x10),
+};
+static struct i2c_board_info i2c_ov7740 = {
+	I2C_BOARD_INFO("ov7740", 0x21),
+};
+
+LINK_SENSOR_MODULE_TO_SOC_CAMERA(ov2640, 0);
+LINK_SENSOR_MODULE_TO_SOC_CAMERA(ov5642, 1);
+LINK_SENSOR_MODULE_TO_SOC_CAMERA(ov9740, 2);
+LINK_SENSOR_MODULE_TO_SOC_CAMERA(ov7740, 3);
+
+static struct platform_device *sensors[] __initdata = {
+	&isi_ov2640,
+	&isi_ov5642,	/* compatible for ov5640 */
+	&isi_ov9740,
+	&isi_ov7740,
+};
+
+struct of_dev_auxdata at91_auxdata_lookup[] __initdata = {
+	OF_DEV_AUXDATA("atmel,at91sam9x5-lcd", 0xf8038000, "atmel_hlcdfb_base", &ek_lcdc_data),
+	OF_DEV_AUXDATA("atmel,at91sam9x5-lcd", 0xf8038100, "atmel_hlcdfb_ovl1", &ek_lcdc_data),
+	OF_DEV_AUXDATA("atmel,at91sam9x5-lcd", 0xf0030000, "atmel_hlcdfb_base", &ek_lcdc_data),
+	OF_DEV_AUXDATA("atmel,at91sam9x5-lcd", 0xf0030140, "atmel_hlcdfb_ovl1", &ek_lcdc_data),
+	OF_DEV_AUXDATA("atmel,at91sam9x5-lcd", 0xf0030240, "atmel_hlcdfb_ovl2", &ek_lcdc_data),
+	OF_DEV_AUXDATA("atmel,at91sam9g45-isi", 0xf0034000, "atmel_isi", &isi_data),
+	{ /* sentinel */ }
+};
 
 static const struct of_device_id irq_of_match[] __initconst = {
 
@@ -60,13 +284,101 @@ static int ksz9021rn_phy_fixup(struct phy_device *phy)
 	return 0;
 }
 
+static void mmd_write_reg(struct phy_device *dev, int device, int reg, int val)
+{
+	phy_write(dev, 0x0d, device);
+	phy_write(dev, 0x0e, reg);
+	phy_write(dev, 0x0d, (1 << 14) | device);
+	phy_write(dev, 0x0e, val);
+}
+
+static int ksz9031rn_phy_fixup(struct phy_device *dev)
+{
+	/*
+	 * min rx data delay, max rx/tx clock delay,
+	 * min rx/tx control delay
+	 */
+	/*
+	mmd_write_reg(dev, 2, 4, 0);
+	mmd_write_reg(dev, 2, 5, 0);
+	mmd_write_reg(dev, 2, 8, 0x003ff);
+	*/
+	mmd_write_reg(dev, 2, 4, 0x84);
+	mmd_write_reg(dev, 2, 5, 0x4444);
+	mmd_write_reg(dev, 2, 8, 0x1ef);
+
+	return 0;
+}
+
 static void __init sama5_dt_device_init(void)
 {
-	if (of_machine_is_compatible("atmel,sama5d3xcm"))
+	struct device_node *np;
+
+	if (of_machine_is_compatible("atmel,sama5d3xcm") &&
+	    IS_ENABLED(CONFIG_PHYLIB))
 		phy_register_fixup_for_uid(PHY_ID_KSZ9021, MICREL_PHY_ID_MASK,
 			ksz9021rn_phy_fixup);
+	else if (of_machine_is_compatible("atmel,sama5d3-xplained") &&
+	    IS_ENABLED(CONFIG_PHYLIB)) {
+		phy_register_fixup_for_uid(PHY_ID_KSZ9031, MICREL_PHY_ID_MASK,
+			ksz9031rn_phy_fixup);
+	}
 
-	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
+	np = of_find_compatible_node(NULL, NULL, "atmel,at91sam9g45-isi");
+	if (np) {
+		if (of_device_is_available(np)) {
+			camera_set_gpio_pins(AT91_PIN_PE24, AT91_PIN_PE29);
+			at91_config_isi(true, "pck1");
+		}
+	}
+
+	/* Hack for PDA display modules to update lcd settings */
+	if (of_machine_is_compatible("pda,tm70xx")) {
+		__u8 manufacturer[4] = "PALM";
+		__u8 monitor[14] = "AT07";
+
+		/* set LCD configuration */
+		at91_tft_vga_modes[0].name = "PALM";
+		at91_tft_vga_modes[0].left_margin = 128;
+		at91_tft_vga_modes[0].right_margin = 0;
+		at91_tft_vga_modes[0].upper_margin = 23;
+		at91_tft_vga_modes[0].lower_margin = 22;
+		at91_tft_vga_modes[0].hsync_len = 5;
+		at91_tft_vga_modes[0].vsync_len = 5;
+
+		memcpy(at91fb_default_monspecs.manufacturer, manufacturer, 4);
+		memcpy(at91fb_default_monspecs.monitor, monitor, 14);
+
+		ek_lcdc_data.default_lcdcon2 = LCDC_LCDCFG5_MODE_OUTPUT_18BPP;
+
+		printk("LCD parameters updated for PDA7 display module\n");
+	}
+	if (of_machine_is_compatible("pda,tm43xx")) {
+		__u8 manufacturer[4] = "Inlx";
+		__u8 monitor[14] = "AT043TN24";
+
+		/* set LCD configuration */
+		at91_tft_vga_modes[0].name = "Inlx";
+		at91_tft_vga_modes[0].xres = 480;
+		at91_tft_vga_modes[0].yres = 272;
+		at91_tft_vga_modes[0].pixclock = KHZ2PICOS(9000);
+		at91_tft_vga_modes[0].left_margin = 2;
+		at91_tft_vga_modes[0].right_margin = 2;
+		at91_tft_vga_modes[0].upper_margin = 2;
+		at91_tft_vga_modes[0].lower_margin = 2;
+		at91_tft_vga_modes[0].hsync_len = 41;
+		at91_tft_vga_modes[0].vsync_len = 11;
+
+		memcpy(at91fb_default_monspecs.manufacturer, manufacturer, 4);
+		memcpy(at91fb_default_monspecs.monitor, monitor, 14);
+
+		ek_lcdc_data.smem_len = 480 * 272 * 4;
+
+		printk("LCD parameters updated for PDA4 display module\n");
+	}
+
+	of_platform_populate(NULL, of_default_bus_match_table, at91_auxdata_lookup, NULL);
+	platform_add_devices(sensors, ARRAY_SIZE(sensors));
 }
 
 static const char *sama5_dt_board_compat[] __initdata = {
