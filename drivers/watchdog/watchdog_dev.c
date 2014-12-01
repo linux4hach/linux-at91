2036fc852705a025525e6cfd1b4c7e455b7cc4b4
ryang <ryang@hach.com>
2014-12-01 10:17:35 -0700

    Fixed the magic watchdog close logic.

--- drivers/watchdog/watchdog_dev.c
+++ drivers/watchdog/watchdog_dev.c
@@ -466,13 +466,13 @@ static int watchdog_release(struct inode *inode, struct file *file)
 
 	/*
 	 * We only stop the watchdog if we received the magic character
-	 * or if WDIOF_MAGICCLOSE is not set. If nowayout was set then
+	 * and WDIOF_MAGICCLOSE is  set. If nowayout was set then
 	 * watchdog_stop will fail.
 	 */
 	if (!test_bit(WDOG_ACTIVE, &wdd->status))
 		err = 0;
-	else if (test_and_clear_bit(WDOG_ALLOW_RELEASE, &wdd->status) ||
-		 !(wdd->info->options & WDIOF_MAGICCLOSE))
+	else if (test_and_clear_bit(WDOG_ALLOW_RELEASE, &wdd->status) &&
+		 (wdd->info->options & WDIOF_MAGICCLOSE))
 		err = watchdog_stop(wdd);
 
 	/* If the watchdog was not stopped, send a keepalive ping */
