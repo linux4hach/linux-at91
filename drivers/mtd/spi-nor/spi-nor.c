48af78cf642822b03020e9ae496422887706122e
cfair <cfair@hach.com>
2016-07-25 09:36:19 -0600

    added a prototype for spi-nor-write so that the reset-device can use it

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -77,6 +77,7 @@ struct flash_info {
 };
 
 #define JEDEC_MFR(info)	((info)->id[0])
+static int spi_nor_write(struct mtd_info *mtd, loff_t to, size_t len, size_t *retlen, const u_char *buf);
 
 static const struct flash_info *spi_nor_match_id(const char *name);
 
