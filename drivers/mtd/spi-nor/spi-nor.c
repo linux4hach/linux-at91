d5b02fb38a58d2d434b9a621dea6b16df0756e07
cfair <cfair@hach.com>
2016-08-23 15:49:51 -0600

    modified:   spi-nor.c

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -364,6 +364,22 @@ static int spi_nor_lock_and_prep(struct spi_nor *nor, enum spi_nor_ops ops)
 	return ret;
 }
 
+
+static int reset_device(struct spi_nor *nor)
+{
+	if (spi_nor_wait_till_ready(nor))
+		return 1;
+
+	flash_cmd_buf[0] = SPINOR_OP_RESET_ENABLE;
+	spi_nor_write(nor, nor->cmd_buf, 1);
+	cond_resched();
+	spi_nor_write(nor, nor->cmd_buf, 1);
+
+	return 0;
+}
+
+
+
 static void spi_nor_unlock_and_unprep(struct spi_nor *nor, enum spi_nor_ops ops)
 {
 	if (nor->unprepare)
