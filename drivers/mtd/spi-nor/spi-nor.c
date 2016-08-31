77d471e7c2c634a1f15a85217862205396bf9f8e
cfair <cfair@hach.com>
2016-08-31 10:19:40 -0600

    updated spi-nor.c

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -526,7 +526,7 @@ static int stm_is_locked_sr(struct spi_nor *nor, loff_t ofs, uint64_t len,
 static int stm_lock(struct spi_nor *nor, loff_t ofs, uint64_t len)
 {
 
-	struct mtd_info *mtd = &nor->mtd_sr;
+	struct mtd_info *mtd = &nor->mtd;
 	u8 status_old, status_new;
 	u8 mask = SR_BP2 | SR_BP1 | SR_BP0;
 	u8 shift = ffs(mask) - 1, pow, val;
