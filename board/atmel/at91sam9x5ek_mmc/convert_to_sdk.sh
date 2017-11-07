8aed2ce2b6bad15351bb1b2280c289912d00849e
jrobot <jrobot@hach.com>
2017-11-07 09:49:09 -0700

    mispelled fs as fw changed back to fs

--- board/atmel/at91sam9x5ek_mmc/convert_to_sdk.sh
+++ board/atmel/at91sam9x5ek_mmc/convert_to_sdk.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo "Converting buildroot into a bare sdk for downloading"
 cd /opt/hachdev/sdk/r1702/buildroot
-rm -rf fw arch board boot configs dl docs linux package support system toolchain utils
+rm -rf fs arch board boot configs dl docs linux package support system toolchain utils
 rm *
 cd /opt/hachdev/sdk/r1702/buildroot/output
 rm -rf build images staging target
